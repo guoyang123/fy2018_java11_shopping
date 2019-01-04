a01c6f5bbe877f2c124d1536835694aa22dd382d
1035610621@qq.com <1035610621@qq.com>
2019-01-04 16:04:13 +0800

    first commit

---
+++ src/main/java/com/neuedu/Fy2018Java11ShoppingApplication.java
@@ -0,0 +1,14 @@
+package com.neuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fy2018Java11ShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fy2018Java11ShoppingApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/neuedu/Fy2018Java11ShoppingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.neuedu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Fy2018Java11ShoppingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
